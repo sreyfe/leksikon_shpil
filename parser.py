3bcf69e04776e53bafa0303f748d641424557aea
sreyfe <jonah0lubin@gmail.com>
2025-01-30 16:15:19 -0500

    combined parser and sequencer

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import json
+import random
 from nltk.tokenize import PunktSentenceTokenizer
 
 with open('data.json') as f:
@@ -31,4 +32,29 @@ for entry in leksikon:
     new_leksikon.append({"name": name, "content": censored_split})
 
 with open('leksikon.json', 'w') as f:
+    #f.write("leksikon = ")
     json.dump(new_leksikon, f, indent=4)
+
+# random sequence of entries with bios > 5 lines
+with open('leksikon.json') as f:
+    leksikon = json.load(f)
+sequence = []
+for entry in leksikon:
+    if len(entry["content"]) >= 5:
+        # random 6 name options for quiz, no bio restrictions
+        options = []
+        for i in range(6):
+            num = random.randrange(len(leksikon))
+            while num == leksikon.index(entry) or num in options:
+                num = random.randrange(len(leksikon))
+            options.append(num)
+        # get random sequence of lines from bio
+        lines = list(range(0, len(entry["content"])))
+        random.shuffle(lines)
+        lines = lines[:5]
+        sequence.append([leksikon.index(entry), options, lines])
+random.shuffle(sequence)
+
+with open('sequence.json', 'w') as f:
+    f.write("sequence = ")
+    json.dump(sequence, f, indent=4)
